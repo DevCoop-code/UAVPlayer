6b7c9ae53177b867212f2e0042ca5092a141252b
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-03-16 13:10:43 +0900

    feat: Make a scene & material & scripts
    Make
    Scene: basicPlaybackScene
    Material: basicPlayTest
    Scripts: UAVPTest, UAVPlayer

---
+++ Unity/UAVPlayer/Assets/Scripts/UAVPTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UAVPTest : MonoBehaviour
+{
+    public Material videoMat = null;
+    public string videoPath = null;
+
+    private bool videoMatTexAssigned = false;
+
+    private UAVPlayer player = new UAVPlayer();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoMat.mainTexture = null;
+        player.play(videoPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!videoMatTexAssigned && player.videoTexture)
+        {
+            videoMat.mainTexture = player.videoTexture;
+            videoMatTexAssigned = true;
+        }
+        if(videoMatTexAssigned && !player.videoTexture)
+        {
+            videoMat.mainTexture = null;
+            videoMatTexAssigned = false;
+        }
+    }
+}
---
+++ Unity/UAVPlayer/Assets/UAVPlayer/UAVPlayer.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class UAVPlayer
+{
+#if UNITY_IPHONE && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern bool UAVP_CanOutputToTexture(string videoPath);
+
+    [DllImport("__Internal")]
+    private static extern bool UAVP_PlayerReady();
+
+    [DllImport("__Internal")]
+    private static extern float UAVP_DurationSeconds();
+
+    [DllImport("__Internal")]
+    private static extern void UAVP_VideoExtents(ref int w, ref int h);
+
+    [DllImport("__Internal")]
+    private static extern System.IntPtr UAVP_CurFrameTexture();
+
+    [DllImport("__Internal")]
+    private static extern bool UAVP_PlayVideo(string videoPath);
+#else
+    private static bool UAVP_CanOutputToTexture(string videoPath) { return false;  }
+
+    private static bool UAVP_PlayerReady() { return false;  }
+
+    private static float UAVP_DurationSeconds() { return 0.0f; }
+
+    private static void UAVP_VideoExtents(ref int w, ref int h) { }
+
+    private static System.IntPtr UAVP_CurFrameTexture() { return System.IntPtr.Zero; }
+
+    private static bool UAVP_PlayVideo(string videoPath) { return false;  }
+#endif
+
+    /*
+     *Properties
+     */
+    public bool videoReady
+    {
+        get
+        {
+            return UAVP_PlayerReady();
+        }
+    }
+
+    public float videoDuration
+    {
+        get
+        {
+            return UAVP_DurationSeconds();
+        }
+    }
+
+    public int videoWidth
+    {
+        get
+        {
+            int w = 0, h = 0;
+            UAVP_VideoExtents(ref w, ref h);
+            return w;
+        }
+    }
+
+    public int videoHeight
+    {
+        get
+        {
+            int w = 0, h = 0;
+            UAVP_VideoExtents(ref w, ref h);
+            return h;
+        }
+    }
+
+    private Texture2D _videoTexture = null;
+    public Texture2D videoTexture
+    {
+        get
+        {
+            System.IntPtr nativeTex = videoReady ? UAVP_CurFrameTexture() : System.IntPtr.Zero;
+            if(nativeTex != System.IntPtr.Zero)
+            {
+                if(_videoTexture == null)
+                {
+                    _videoTexture = Texture2D.CreateExternalTexture(videoWidth, videoHeight, TextureFormat.BGRA32, false, false, nativeTex);
+                    _videoTexture.filterMode = FilterMode.Bilinear;
+                    _videoTexture.wrapMode = TextureWrapMode.Repeat;
+                }
+
+                _videoTexture.UpdateExternalTexture(nativeTex);
+            }
+            else
+            {
+                _videoTexture = null;
+            }
+
+            return _videoTexture;
+        }
+    }
+
+    /*
+     *Functions
+     */
+     public static bool CanOutputToTexture(string videoPath)
+    {
+        return UAVP_CanOutputToTexture(videoPath);
+    }
+
+    public void play(string videoPath)
+    {
+        if (CanOutputToTexture(videoPath))
+            UAVP_PlayVideo(videoPath);
+    }
+}
