baac208d28defcfac32bea523cc9838063a8d350
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-05-31 15:26:03 +0900

    feat: Make UAVPFoundation, UAVPTypes

---
+++ Unity/UAVPlayer/Assets/UAVP/UAVPSDK/UAVPFoundation.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UAVPFoundation
+{   
+    public bool mute;                       // Set whether to turn off the audio or not
+    public bool loop;                       // Set the when the video reaches the end poosition it jumps to the start position and plays again
+    public bool autoPlayback;               // Set the playing video automatically when app is started
+    public UAVPLogLevel logLevelForDebugging;   // Set Log level for debugging
+    // Set the audio volume
+    private float _volume = 1.0f;
+    public float volume
+    {
+        get
+        {
+            return _volume;
+        }
+        set
+        {
+            if(_volume > 100.0f)
+            {
+                _volume = 1.0f;
+            }
+            else if(_volume < 0.0f)
+            {
+                _volume = 0.0f;
+            }
+            else
+            {
+                _volume = value / 100.0f;
+            }
+        }
+    }
+
+    /*
+    Initializes the player
+    - logLevel: Set the LogLevel(Debug, Source, Porting, System)
+    */
+    public virtual UAVPError InitPlayer(UAVPLogLevel logLevel) { return UAVPError.UAVP_ERROR_NONE; }
+
+    /*
+    Open the media with player
+    - URI: Media Location 
+    - mediaType: Types of media
+    */
+    public virtual UAVPError OpenMediaStreaming(string URI, UAVPMediaType mediaType) { return UAVPError.UAVP_ERROR_NONE; }
+
+    /*
+    Playing the Media
+    */
+    abstract public void Start();
+
+    /*
+    Pause the Media
+    */
+    abstract public void Pause();
+
+    /*
+    Resumes the Media
+    */
+    abstract public void Resume();
+
+    /*
+    Release the Player
+    */
+    abstract public void Release();
+
+    /*
+    Returns the current time of the playback
+    - return: Time currently being played
+    */
+    abstract public int GetCurrentTime();
+
+    /*
+    Returns the whole playable time of the media
+    - return: Whole playable time
+    */
+    abstract public int GetTotalTime();
+}
---
+++ Unity/UAVPlayer/Assets/UAVP/UAVPSDK/UAVPTypes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Debugging Log Level
+public enum UAVPLogLevel
+{
+    Debug = 0,      // Level of UAVP Script
+    Source = 1,     // Level of Unity Script
+    Porting = 2,    // Level of Plugin
+    System = 3      // Level of Frameworks
+};
+
+// Error Types
+public enum UAVPError
+{
+    UAVP_ERROR_NONE = 0         // No Error
+}
+
+// Media Types
+public enum UAVPMediaType
+{
+    UAVP_Streaming_Media = 0,       // Streaming Media(like HLS, Dash, ....)
+    UAVP_Local_Media = 1            // Local Media(Media in StreamingAssets Directory)
+}
