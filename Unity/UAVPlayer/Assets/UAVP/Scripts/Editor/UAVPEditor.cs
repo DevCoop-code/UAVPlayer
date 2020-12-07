abd3b453eff8ae38f714f8ab62668fb957b321bd
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-12-08 01:03:33 +0900

    refactor: Change Customize UAVP Script

---
+++ Unity/UAVPlayer/Assets/UAVP/Scripts/Editor/UAVPEditor.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UAVPAPI;
+using System.IO;
+using System.Linq;
+
+namespace UAVPAPI
+{
+    [CustomEditor(typeof(UAVP))]
+    public class UAVPEditor : Editor
+    {
+        SerializedProperty _autoPlay;
+        SerializedProperty _loop;
+        SerializedProperty _mute;
+        SerializedProperty _videoMat;
+        SerializedProperty _videoRawImage;
+        SerializedProperty _elapsedTime;
+        SerializedProperty _totalTime;
+        SerializedProperty _seekbar;
+        SerializedProperty _mediaURI;
+        SerializedProperty _logLevel;
+        SerializedProperty _mediaPlayType;
+
+        private string[] _playType;
+
+        private void OnEnable()
+        {
+            _autoPlay       = serializedObject.FindProperty("autoPlay");
+            _loop           = serializedObject.FindProperty("loop");
+            _mute           = serializedObject.FindProperty("mute");
+            _videoMat       = serializedObject.FindProperty("videoMat");
+            _videoRawImage  = serializedObject.FindProperty("videoRawImage");
+            _elapsedTime    = serializedObject.FindProperty("elapsedTime");
+            _totalTime      = serializedObject.FindProperty("totalTime");
+            _seekbar        = serializedObject.FindProperty("seekbar");
+            _mediaURI       = serializedObject.FindProperty("mediaURI");
+            _logLevel       = serializedObject.FindProperty("logLevel");
+            _mediaPlayType  = serializedObject.FindProperty("mediaPlayType");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            UAVP uavpEditor = (UAVP)target;
+
+            EditorGUI.BeginChangeCheck();
+
+            // Additional Fields
+            EditorGUILayout.Space();
+            EditorStyles.label.fontStyle = FontStyle.Bold;
+            EditorGUILayout.LabelField("Additional Properties:");
+
+            GUILayout.BeginVertical("Box");
+
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Auto Play:", GUILayout.MinWidth(10));
+            _autoPlay.boolValue = EditorGUILayout.Toggle(_autoPlay.boolValue, EditorStyles.radioButton);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Loop:", GUILayout.MinWidth(10));
+            _loop.boolValue = EditorGUILayout.Toggle(_loop.boolValue, EditorStyles.radioButton);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Mute:", GUILayout.MinWidth(10));
+            _mute.boolValue = EditorGUILayout.Toggle(_mute.boolValue, EditorStyles.radioButton);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            int[] logOptions = {0, 1, 2, 3};
+            string[] logOptionsText = { "Debug", "Source", "Porting", "System" };
+            _logLevel.intValue = EditorGUILayout.IntPopup("LogLevel", _logLevel.intValue, logOptionsText, logOptions);
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+
+            _playType = new string[] { "Media Streaming", "Local", "Asset" };
+            _mediaPlayType.intValue = GUILayout.SelectionGrid(_mediaPlayType.intValue, _playType, _playType.Length, EditorStyles.miniButton);
+            _mediaPlayType.intValue = Mathf.Clamp(_mediaPlayType.intValue, 0, _playType.Length);
+
+            // Streaming Video Field
+            if (_mediaPlayType.intValue == 0)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("URL : ");
+                EditorStyles.textField.wordWrap = true;
+
+                _mediaURI.stringValue = EditorGUILayout.TextField(_mediaURI.stringValue, GUILayout.Height(30));
+                EditorStyles.textField.wordWrap = false;
+            }
+
+            // Local Video Field
+            if (_mediaPlayType.intValue == 1)
+            {
+                
+            }
+
+            // Asset Video Path Field
+            if (_mediaPlayType.intValue == 2)
+            {
+                
+            }
+
+            // Rendering Field
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Objects for which video will be Rendered");
+            GUILayout.BeginVertical("Box");
+            EditorGUILayout.PropertyField(_videoMat, new GUIContent("Material"), true);
+            EditorGUILayout.PropertyField(_videoRawImage, new GUIContent("RawImage"), true);
+            GUILayout.EndVertical();
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Media Control UI");
+            GUILayout.BeginVertical("Box");
+            EditorGUILayout.PropertyField(_elapsedTime, new GUIContent("Current Time"), true);
+            EditorGUILayout.PropertyField(_totalTime, new GUIContent("Media TotalTime"), true);
+            EditorGUILayout.PropertyField(_seekbar, new GUIContent("Media SeekBar"), true);
+            GUILayout.EndVertical();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
--- Unity/UAVPlayer/Assets/UAVP/Scripts/UAVP.cs
+++ Unity/UAVPlayer/Assets/UAVP/Scripts/UAVP.cs
@@ -4,296 +4,312 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class UAVP : MonoBehaviour
+namespace UAVPAPI
 {
-    /*
-    Properties
-    */
-    public bool autoPlay = true;
-    public bool loop = false;
-    public bool mute = false;
-
-    /*
-    Unity Object which is assign Video Texture
-    */
-    public Material videoMat = null;
-    public RawImage videoRawImage = null;
-    public Text elapsedTime;
-    public Text totalTime;
-    public Slider seekbar;
-
-    /*
-    If the media type is Streaming, 
-    mediaURI is like http://~~~~.mpd or .m3u8
-    If the media type is Local or StreamingAssets,
-    mediaURI is like [directoryName]/[FileName].mp4 or .avi
-    */
-    [Tooltip("If you want to play Streaming video, you should input the URL, But if you play the video in StreamingAssets or Local, you should put the Location Path")]
-    public string mediaURI = null;
-
-    public UAVPLogLevel logLevel;
-
-    private bool videoTexAssigned = false;
-    private UAVPlayerSource player = (UAVPlayerSource)UAVPFactory.GetUAVPlayer();
-
-    private bool isSeeking = false;
-
-    // Start is called before the first frame update
-    void Start()
+    public class UAVP : MonoBehaviour
     {
-        // Initialize the property
-        EventTrigger eventTrigger = seekbar.gameObject.AddComponent<EventTrigger>();
+        /*
+        Properties
+        */
+        [SerializeField]
+        public bool autoPlay = true;
+        [SerializeField]
+        public bool loop = false;
+        [SerializeField]
+        public bool mute = false;
 
-        // When Click the Slider
-        EventTrigger.Entry entry_PointerDown = new EventTrigger.Entry();
-        entry_PointerDown.eventID = EventTriggerType.PointerDown;
-        entry_PointerDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
-        eventTrigger.triggers.Add(entry_PointerDown);
+        /*
+        Unity Object which is assign Video Texture
+        */
+        [SerializeField]
+        public Material videoMat = null;
+        [SerializeField]
+        public RawImage videoRawImage = null;
+        [SerializeField]
+        public Text elapsedTime;
+        [SerializeField]
+        public Text totalTime;
+        [SerializeField]
+        public Slider seekbar;
 
-        // When Touch Up the Slider
-        EventTrigger.Entry entry_EndDrag = new EventTrigger.Entry();
-        entry_EndDrag.eventID = EventTriggerType.EndDrag;
-        entry_EndDrag.callback.AddListener((data) => { OnEndDrag((PointerEventData)data); });
-        eventTrigger.triggers.Add(entry_EndDrag);
+        /*
+        If the media type is Streaming, 
+        mediaURI is like http://~~~~.mpd or .m3u8
+        If the media type is Local or StreamingAssets,
+        mediaURI is like [directoryName]/[FileName].mp4 or .avi
+        */
+        [Tooltip("If you want to play Streaming video, you should input the URL, But if you play the video in StreamingAssets or Local, you should put the Location Path")]
+        [SerializeField]
+        public string mediaURI = null;
 
-        if (autoPlay)
+        [SerializeField]
+        public UAVPMediaType mediaPlayType;
+
+        [SerializeField]
+        public UAVPLogLevel logLevel;
+
+        private bool videoTexAssigned = false;
+        private UAVPlayerSource player = (UAVPlayerSource)UAVPFactory.GetUAVPlayer();
+
+        private bool isSeeking = false;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            if (player != null)
-                player.setProperty(UAVPProperty.UAVP_AUTOPLAY, 1);
-        }
-        if (loop)
-        {
-            if (player != null)
-                player.setProperty(UAVPProperty.UAVP_LOOP, 1);
-        }
-        if (mute)
-        {
-            if (player != null)
-                player.setProperty(UAVPProperty.UAVP_MUTE, 1);
-        }
+            // Initialize the property
+            EventTrigger eventTrigger = seekbar.gameObject.AddComponent<EventTrigger>();
 
-        if(seekbar != null)
-        {
-            seekbar.minValue = 0;
-        }
+            // When Click the Slider
+            EventTrigger.Entry entry_PointerDown = new EventTrigger.Entry();
+            entry_PointerDown.eventID = EventTriggerType.PointerDown;
+            entry_PointerDown.callback.AddListener((data) => { OnPointerDown((PointerEventData)data); });
+            eventTrigger.triggers.Add(entry_PointerDown);
 
-        if(videoMat != null)
-        {
-            videoMat.mainTexture = null;
-        }
+            // When Touch Up the Slider
+            EventTrigger.Entry entry_EndDrag = new EventTrigger.Entry();
+            entry_EndDrag.eventID = EventTriggerType.EndDrag;
+            entry_EndDrag.callback.AddListener((data) => { OnEndDrag((PointerEventData)data); });
+            eventTrigger.triggers.Add(entry_EndDrag);
 
-        if(player != null)
-        {
-            // Register the Event
-            UAVPlayerSource.onEvent += EventNotify;
-
-            Debug.Log("Start to play [" + mediaURI + "]");
-            UAVPError error = player.InitPlayer(logLevel);
-            if(error == UAVPError.UAVP_ERROR_NONE)
+            if (autoPlay)
             {
-                player.OpenMedia(mediaURI, UAVPMediaType.UAVP_Streaming_Media);
+                if (player != null)
+                    player.setProperty(UAVPProperty.UAVP_AUTOPLAY, 1);
+            }
+            if (loop)
+            {
+                if (player != null)
+                    player.setProperty(UAVPProperty.UAVP_LOOP, 1);
+            }
+            if (mute)
+            {
+                if (player != null)
+                    player.setProperty(UAVPProperty.UAVP_MUTE, 1);
+            }
+
+            if(seekbar != null)
+            {
+                seekbar.minValue = 0;
+            }
+
+            if(videoMat != null)
+            {
+                videoMat.mainTexture = null;
+            }
+
+            if(player != null)
+            {
+                // Register the Event
+                UAVPlayerSource.onEvent += EventNotify;
+
+                Debug.Log("Start to play [" + mediaURI + "]");
+                UAVPError error = player.InitPlayer(logLevel);
+                if(error == UAVPError.UAVP_ERROR_NONE)
+                {
+                    player.OpenMedia(mediaURI, mediaPlayType);
+                }
+                else
+                {
+                    player = null;
+                }
             }
             else
             {
-                player = null;
+                Debug.Log("Player is null");
             }
         }
-        else
-        {
-            Debug.Log("Player is null");
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(player != null)
+        // Update is called once per frame
+        void Update()
         {
-            if (!videoTexAssigned && player.videoTexture)
+            if(player != null)
             {
-                Debug.Log("Assign the Texture");
+                if (!videoTexAssigned && player.videoTexture)
+                {
+                    Debug.Log("Assign the Texture");
 
-                if (videoMat != null)
-                    videoMat.mainTexture = player.videoTexture;
+                    if (videoMat != null)
+                        videoMat.mainTexture = player.videoTexture;
 
-                if (videoRawImage != null)
-                    videoRawImage.GetComponent<RawImage>().texture = player.videoTexture;
-                
-                videoTexAssigned = true;
+                    if (videoRawImage != null)
+                        videoRawImage.GetComponent<RawImage>().texture = player.videoTexture;
+                    
+                    videoTexAssigned = true;
+                }
+                if (videoTexAssigned && !player.videoTexture)
+                {
+                    Debug.Log("Release the Texture");
+
+                    if (videoMat != null)
+                        videoMat.mainTexture = null;
+                    if (videoRawImage != null)
+                        videoRawImage.GetComponent<RawImage>().texture = null;
+
+                    videoTexAssigned = false;
+                }
             }
-            if (videoTexAssigned && !player.videoTexture)
+        }
+
+        private void OnDestroy()
+        {
+            if (player != null)
             {
-                Debug.Log("Release the Texture");
+                player.Release();
+            }
 
-                if (videoMat != null)
-                    videoMat.mainTexture = null;
-                if (videoRawImage != null)
-                    videoRawImage.GetComponent<RawImage>().texture = null;
+            player = null;
+        }
 
-                videoTexAssigned = false;
+        /*
+        Player Behaviour
+        */
+        public void OnPlay()
+        {
+            Debug.Log("OnPlay");
+            if (player != null)
+            {
+                player.Start();
             }
         }
-    }
 
-    private void OnDestroy()
-    {
-        if (player != null)
+        public void OnPause()
         {
-            player.Release();
+            Debug.Log("OnPause");
+            if (player != null)
+            {
+                player.Pause();
+            }
         }
 
-        player = null;
-    }
-
-    /*
-    Player Behaviour
-    */
-    public void OnPlay()
-    {
-        Debug.Log("OnPlay");
-        if (player != null)
+        public void OnSeek()
         {
-            player.Start();
-        }
-    }
-
-    public void OnPause()
-    {
-        Debug.Log("OnPause");
-        if (player != null)
-        {
-            player.Pause();
-        }
-    }
-
-    public void OnSeek()
-    {
-        Debug.Log("OnSeek");
-        if (player != null && seekbar != null)
-        {
-            player.Seek((int)seekbar.value);
-        }
-    }
-
-    public void ToggleStartPause()
-    {
-        Debug.Log("OnToggleStartPause");
-        switch(player.playerStatus)
-        {
-            case UAVPStatus.UAVP_START:
-                {
-                    OnPause();
-                }
-            break;
-
-            case UAVPStatus.UAVP_PAUSE:
-                {
-                    OnPlay();
-                }
-            break;
-
-            case UAVPStatus.UAVP_OPEN:
-                {
-                    OnPlay();
-                }
-            break;
-        }
-    }
-
-    void OnPointerDown(PointerEventData data)
-    {
-        Debug.Log("Seekbar Pointer Down");
-
-        isSeeking = true;
-    }
-
-    void OnEndDrag(PointerEventData data)
-    {
-        Debug.Log("Seekbar Pointer Up");
-
-        OnSeek();
-
-        isSeeking = false;
-    }
-
-    // UAVP Event
-    public void EventNotify(int type, float param1, float param2, float param3)
-    {
-        if (type != 1) 
-        {
-            Debug.Log("EventNotify, type: " + type + " ,param1: " + param1 + " ,param2: " + param2 + " ,param3: " + param3);
+            Debug.Log("OnSeek");
+            if (player != null && seekbar != null)
+            {
+                player.Seek((int)seekbar.value);
+            }
         }
 
-        switch (type)
+        public void ToggleStartPause()
         {
-            case 0:     // Total Media Time(seconds)
-                string t_minuteStr = "00";
-                string t_secondStr = "00";
+            Debug.Log("OnToggleStartPause");
+            switch(player.playerStatus)
+            {
+                case UAVPStatus.UAVP_START:
+                    {
+                        OnPause();
+                    }
+                break;
 
-                float totalTimeSeconds = param1;
-                int t_hour = (int)totalTimeSeconds / 3600;
-                int t_minute = (int)(totalTimeSeconds % 3600) / 60;
-                int t_second = (int)(totalTimeSeconds % 3600) % 60;
-                if(t_minute >=0 && t_minute < 10)
-                {
-                    t_minuteStr = "0" + t_minute.ToString();
-                }
-                else
-                {
-                    t_minuteStr = t_minute.ToString();
-                }
+                case UAVPStatus.UAVP_PAUSE:
+                    {
+                        OnPlay();
+                    }
+                break;
 
-                if(t_second >= 0 && t_second < 10)
-                {
-                    t_secondStr = "0" + t_second.ToString();
-                }
-                else
-                {
-                    t_secondStr = t_second.ToString();
-                }
-                totalTime.text = t_hour.ToString() + ":" + t_minuteStr + ":" + t_secondStr;
+                case UAVPStatus.UAVP_OPEN:
+                    {
+                        OnPlay();
+                    }
+                break;
+            }
+        }
 
-                if(seekbar != null)
-                    seekbar.maxValue = totalTimeSeconds;
-            break;
+        void OnPointerDown(PointerEventData data)
+        {
+            Debug.Log("Seekbar Pointer Down");
 
-            case 1:     // Current Media Time(seconds)
-                string e_minuteStr = "00";
-                string e_secondStr = "00";
+            isSeeking = true;
+        }
 
-                float elapsedTimeSeconds = param1;
-                int e_hour = (int)elapsedTimeSeconds / 3600;
-                int e_minute = (int)(elapsedTimeSeconds % 3600) / 60;
-                int e_second = (int)(elapsedTimeSeconds % 3600) % 60;
-                if(e_minute >=0 && e_minute < 10)
-                {
-                    e_minuteStr = "0" + e_minute.ToString();
-                }
-                else
-                {
-                    e_minuteStr = e_minute.ToString();
-                }
+        void OnEndDrag(PointerEventData data)
+        {
+            Debug.Log("Seekbar Pointer Up");
 
-                if(e_second >= 0 && e_second < 10)
-                {
-                    e_secondStr = "0" + e_second.ToString();
-                }
-                else
-                {
-                    e_secondStr = e_second.ToString();
-                }
-                elapsedTime.text = e_hour.ToString() + ":" + e_minuteStr + ":" + e_secondStr;
+            OnSeek();
 
-                if(seekbar != null)
-                {
-                    if (!isSeeking)
-                        seekbar.value = elapsedTimeSeconds;
-                }
-            break;
+            isSeeking = false;
+        }
 
-            default:
+        // UAVP Event
+        public void EventNotify(int type, float param1, float param2, float param3)
+        {
+            if (type != 1) 
+            {
+                Debug.Log("EventNotify, type: " + type + " ,param1: " + param1 + " ,param2: " + param2 + " ,param3: " + param3);
+            }
 
-            break;
+            switch (type)
+            {
+                case 0:     // Total Media Time(seconds)
+                    string t_minuteStr = "00";
+                    string t_secondStr = "00";
+
+                    float totalTimeSeconds = param1;
+                    int t_hour = (int)totalTimeSeconds / 3600;
+                    int t_minute = (int)(totalTimeSeconds % 3600) / 60;
+                    int t_second = (int)(totalTimeSeconds % 3600) % 60;
+                    if(t_minute >=0 && t_minute < 10)
+                    {
+                        t_minuteStr = "0" + t_minute.ToString();
+                    }
+                    else
+                    {
+                        t_minuteStr = t_minute.ToString();
+                    }
+
+                    if(t_second >= 0 && t_second < 10)
+                    {
+                        t_secondStr = "0" + t_second.ToString();
+                    }
+                    else
+                    {
+                        t_secondStr = t_second.ToString();
+                    }
+                    totalTime.text = t_hour.ToString() + ":" + t_minuteStr + ":" + t_secondStr;
+
+                    if(seekbar != null)
+                        seekbar.maxValue = totalTimeSeconds;
+                break;
+
+                case 1:     // Current Media Time(seconds)
+                    string e_minuteStr = "00";
+                    string e_secondStr = "00";
+
+                    float elapsedTimeSeconds = param1;
+                    int e_hour = (int)elapsedTimeSeconds / 3600;
+                    int e_minute = (int)(elapsedTimeSeconds % 3600) / 60;
+                    int e_second = (int)(elapsedTimeSeconds % 3600) % 60;
+                    if(e_minute >=0 && e_minute < 10)
+                    {
+                        e_minuteStr = "0" + e_minute.ToString();
+                    }
+                    else
+                    {
+                        e_minuteStr = e_minute.ToString();
+                    }
+
+                    if(e_second >= 0 && e_second < 10)
+                    {
+                        e_secondStr = "0" + e_second.ToString();
+                    }
+                    else
+                    {
+                        e_secondStr = e_second.ToString();
+                    }
+                    elapsedTime.text = e_hour.ToString() + ":" + e_minuteStr + ":" + e_secondStr;
+
+                    if(seekbar != null)
+                    {
+                        if (!isSeeking)
+                            seekbar.value = elapsedTimeSeconds;
+                    }
+                break;
+
+                default:
+
+                break;
+            }
         }
     }
 }
